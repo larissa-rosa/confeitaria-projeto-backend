938828ee3157179244fd7bb583cea1cb66a0275a
Larissa Rosa <larissa_rosa7@hotmail.com>
2022-05-23 20:11:17 -0300

    configurado o spring boot

---
+++ src/test/java/br/com/confeitaria/confeitaria/ConfeitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.confeitaria.confeitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
